dc023a5ea87ef75306d09e2525020e617b832deb
Jaya <jaya.dev100x@gmail.com>
2025-06-26 22:31:01 +0530

    env setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 
 const app = express();
+//@ts-ignore
+import { envLoader } from '@repo/shared/config'
 
+envLoader();
 
-app.post('/signin')
+// app.post('/signin')
 
-app.post('/signup')
+// app.post('/signup')
 
-app.post('/room')
-
+// app.post('/room')
+console.log(process.env.JWT_SECRET)
 app.listen(3001)
--- apps/http-backend/src/middlewares/auth.ts
+++ apps/http-backend/src/middlewares/auth.ts
@@ -2,8 +2,6 @@ import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import 'dotenv/config';
 
-// import { JWT_SECRET } from "@repo/shared/config"
-
 export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers['authorization'] ?? "";
 
@@ -11,8 +9,7 @@ export const userMiddleware = (req: Request, res: Response, next: NextFunction) 
         return res.status(403).json({message: "Invalid token!"})
     }
 
-    
-
+  
 
 }
 
