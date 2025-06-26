5c44f2497c1bc62885f420803d8b6685a7e4b97d
Jaya <jaya.dev100x@gmail.com>
2025-06-26 19:44:52 +0530

    minor changes

--- apps/http-backend/src/middlewares/auth.ts
+++ apps/http-backend/src/middlewares/auth.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import 'dotenv/config';
 
-import { JWT_SECRET } from "@repo/shared/config"
+// import { JWT_SECRET } from "@repo/shared/config"
 
 export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers['authorization'] ?? "";
